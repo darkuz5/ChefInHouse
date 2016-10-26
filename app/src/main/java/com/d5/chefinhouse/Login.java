1e0c963eb666f70dad02a0c0c23f662756183808
Oscar Adan <elmaildeoscaradan@gmail.com>
2016-10-25 19:50:56 -0500

    Configuracion para login con facebook

---
+++ app/src/main/java/com/d5/chefinhouse/Login.java
@@ -0,0 +1,14 @@
+package com.d5.chefinhouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.facebook.*;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
